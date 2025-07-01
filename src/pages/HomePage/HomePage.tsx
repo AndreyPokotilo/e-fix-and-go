1eab72224c5c46490730b55095db06f4a08430b8
AndreyPokotilo <dpon444@gmail.com>
2025-07-01 13:02:13 +0200

    tets commit

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import { HomeContainer }from './HomePage.styled.js';
+// import { StartContent } from 'components/StartContent/StartContent';
+
+export function HomePage() {
+  return (
+    <HomeContainer>
+      {/* <StartContent/> */}
+    </HomeContainer>
+  );
+}
