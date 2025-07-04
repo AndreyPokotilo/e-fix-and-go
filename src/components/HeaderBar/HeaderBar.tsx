c81f297be94501db7b154de255a2fcb543523816
AndreyPokotilo <dpon444@gmail.com>
2025-07-04 21:05:56 +0200

    ашчув ифпы

---
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -0,0 +1,10 @@
+import { HeaderContainer } from "./HeaderBar.styled";
+
+export function HeaderBar() {
+  
+  return (
+    <HeaderContainer>
+      <h1>Header</h1>
+    </HeaderContainer>
+  );
+}
